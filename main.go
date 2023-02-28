3d20f20706199911c7f02663eec8bd725c43b122
Sam <m.r.sambaga@gmail.com>
2023-02-28 21:31:59 +0700

    add post method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,8 +11,10 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id", booksHandler)
+	router.GET("query", queryHandler)
+	router.POST("/books", postBooksHandler)
 
 	router.Run()
 }
@@ -27,5 +30,39 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":    "Hello World",
 		"subtitle": "Golang Web API Tutorial",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id") // untuk path (books/1)
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title") //untuk query string (query?tittle="example")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "price": price})
+}
+
+type BookInput struct {
+	Title    string
+	Price    int
+	Subtitle string `json:"sub_title"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"title":     bookInput.Title,
+		"price":     bookInput.Price,
+		"sub_title": bookInput.Subtitle,
 	})
 }
