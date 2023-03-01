8953fbee120f87380a9c540640d3930bf445629d
Sam <m.r.sambaga@gmail.com>
2023-03-01 10:26:16 +0700

    connect to mysql database & create data

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	router := gin.Default()
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Db connection error")
+	}
 
+	fmt.Println("Database connecction succeed")
+
+	db.AutoMigrate(&book.Book{})
+
+	book := book.Book{}
+	book.Title = "Man Tiger"
+	book.Price = 90000
+	book.Rating = 5
+	book.Description = "This is a book about tiger-like man"
+
+	err = db.Create(&book).Error
+	if err != nil {
+		fmt.Println("Error creating book record")
+	}
+
+	router := gin.Default()
 	v1 := router.Group("/v1")
 
 	v1.GET("/", handler.RootHandler)
